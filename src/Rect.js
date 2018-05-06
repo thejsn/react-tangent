fb44adfc65c68ccc3d105e2a74dd09005c66e80e
thejsn <thejsn@users.noreply.github.com>
2018-05-06 20:44:22 +0200

    Update Rect component to set pressed and released classes

--- src/Rect.js
+++ src/Rect.js
@@ -1,73 +1,128 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const TOUCH = 'touch';
 const MOUSE = 'mouse';
 
+class Rect extends Component {
+	constructor(props) {
+		super(props);
 
-const Rect = ({
-	width, height, fill, label, id, index,
-	altId, altLabel, borderradius,
-	onClick, onPress, onRelease
-}) => {
-	const class_id = ('' + id) 		||
-					 ('' + label) 	||
-					 ('' + altId) 	||
-					 ('' + altLabel);
-	
-	const className = 'tangent-key tangent-key--index-' + index + ' tangent-key--' + 
-		class_id.replace(/[\~\!\@\$\%\^\&\*\(\)\+\=\,\.\/\'\;\:\"\?\>\<\[\]\\\{\}\|\`\#\s]/g, '-')
-	
-	return (
-		<rect
-			onClick={ () => onClick({
-				label,
-				id,
-				index
-			}) }
-			onTouchStart={ () => onPress({
-				type: TOUCH,
-				label,
-				id,
-				index
-			}) }
-			onTouchEnd={ () => onRelease({
-				type: TOUCH,
-				label,
-				id,
-				index
-			}) }
-			onMouseDown={ () => onPress({
-				type: MOUSE,
-				label,
-				id,
-				index
-			}) }
-			onMouseUp={ () => onRelease({
-				type: MOUSE,
-				label,
-				id,
-				index
-			}) }
-			className={ className }
-			fill={ fill }
-			width={ width }
-			height={ height }
-			data-id={ id }
-			data-label={ label }
-			data-altid={ altId }
-			data-altlabel={ altLabel }
-			rx={ borderradius }
-		></rect>
-	);
-};
+		this.clicked = this.clicked.bind(this);
+		this.pressed = this.pressed.bind(this);
+		this.released = this.released.bind(this);
+
+		this.state = {
+			pressed: false,
+			released: false
+		}
+	}
+
+	clicked(data) {
+		this.props.onClick(data);
+	}
+
+	pressed(data) {
+		
+		this.setState({ pressed: true });
+		setTimeout( () => this.setState({ pressed: false }), 300 );
+		
+		this.props.onPress(data);
+	}
+
+	released(data) {
+		
+		this.setState({ released: true });
+		setTimeout( () => this.setState({ released: false }), 300 );
+		
+		this.props.onRelease(data);
+	}
+
+	render() {
+		const {
+			width,
+			height,
+			fill,
+			label,
+			id,
+			index,
+			altId,
+			altLabel,
+			borderradius,
+			onClick,
+			onPress,
+			onRelease
+		} = this.props;
+
+		const class_id = ('' + id) ||
+			('' + label) ||
+			('' + altId) ||
+			('' + altLabel);
+
+		const keyStateClass = (
+			this.state.pressed ? ' tangent-key--pressed' : 
+			this.state.released ? ' tangent-key--released' :
+			' tangent-key--idle'
+		);
+
+		const className = 'tangent-key' +
+			keyStateClass +
+			(this.state.pressed ? ' tangent-key--pressed' : '') +
+			' tangent-key--index-' + index +
+			' tangent-key--' +
+			class_id.replace(/[\~\!\@\$\%\^\&\*\(\)\+\=\,\.\/\'\;\:\"\?\>\<\[\]\\\{\}\|\`\#\s]/g, '-')
+
+		return (
+			<rect
+				onClick={() => this.clicked({
+					label,
+					id,
+					index
+				})}
+				onTouchStart={() => this.pressed({
+					type: TOUCH,
+					label,
+					id,
+					index
+				})}
+				onTouchEnd={() => this.released({
+					type: TOUCH,
+					label,
+					id,
+					index
+				})}
+				onMouseDown={() => this.pressed({
+					type: MOUSE,
+					label,
+					id,
+					index
+				})}
+				onMouseUp={() => this.released({
+					type: MOUSE,
+					label,
+					id,
+					index
+				})}
+				className={className}
+				fill={fill}
+				width={width}
+				height={height}
+				data-id={id}
+				data-label={label}
+				data-altid={altId}
+				data-altlabel={altLabel}
+				rx={borderradius}
+			></rect>
+		);
+	}
+}
 
 Rect.defaultProps = {
-	altId: '', 
-	altLabel: '', 
+	altId: '',
+	altLabel: '',
 	borderradius: 4,
-	onClick: () => {},
-	onPress: () => {},
-	onRelease: () => {}
+	onClick: () => { },
+	onPress: () => { },
+	onRelease: () => { }
 };
 
 export default Rect;
